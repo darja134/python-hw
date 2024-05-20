affa6971976a8b0cb3d9a6aa9b7ff6587113801a
darja134 <55285599+darja134@users.noreply.github.com>
2024-05-20 17:22:18 +0300

    Updated code style lesson_1_task_7.py

--- lesson_1/lesson_1_task_7.py
+++ lesson_1/lesson_1_task_7.py
@@ -2,6 +2,7 @@
 def print_number(number):
     return str(number)
 
+
 # Print 88005553535 by caling functions
 number = print_number(8) + print_number(8) + print_number(0) + print_number(0) + print_number(5) + \
     print_number(5) + print_number(5) + print_number(5) + print_number(3) + print_number(5) + print_number(3)
@@ -10,6 +11,7 @@ print("Variant no.1: " + number)
 # Variant no.2
 def print_number_2(number):
     print(number, end = "")
+
 
 # Print 88005553535 by caling functions
 print("Variant no.2: ", end = "")
