1ea81dc530ab38e28b3db47839dffce2f13aa4fe
darja134 <55285599+darja134@users.noreply.github.com>
2024-05-29 15:00:47 +0300

    Updated mailing.py (commented fields, check in constructor)

--- lesson3/mailing.py
+++ lesson3/mailing.py
@@ -1,16 +1,16 @@
 from address import Address
 
 class Mailing:
-    to_address = "000000,City,St. Street,13,4"
-    from_address = "000000,City,St. Street,13,4"
-    cost = 0
-    track = "0000000"
+    #to_address = "000000,City,St. Street,13,4"
+    #from_address = "000000,City,St. Street,13,4"
+    #cost = 0
+    #track = "0000000"
 
 
     def __init__(self, to_address, from_address, cost, track):
-        if(not to_address or not from_address or not cost or not track):
-            raise ValueError("Value(s) should not be empty!")
-        else:
+        #if(not to_address or not from_address or not cost or not track):
+        #    raise ValueError("Value(s) should not be empty!")
+        #else:
             self.to_address = Address(to_address.split(","))
             self.from_address = Address(from_address.split(","))
             self.cost = cost
