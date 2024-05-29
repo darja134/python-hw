72d89bc1e76adc044a9de75f0afc2cffad1298b4
darja134 <55285599+darja134@users.noreply.github.com>
2024-05-29 15:32:41 +0300

    Updatde lesson_3_task_1.py (changed methods calls)

--- lesson3/lesson_3_task_1.py
+++ lesson3/lesson_3_task_1.py
@@ -2,11 +2,11 @@ from user import User
 
 my_user = User("Darja", "Zorina")
 
-my_user.printName()
-my_user.printSurname()
-my_user.printNameAnsSurname()
+my_user.print_mame()
+my_user.print_surname()
+my_user.print_name_and_surname()
 
 
-my_user_error_2 = User("Darja", "")
+#my_user_error_2 = User("Darja", "")
 #my_user_error_1 = User("", "Zorina")
 #my_user_error_3 = User("", "")
