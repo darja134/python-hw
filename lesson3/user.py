e8f1a92a753c15ff9e43f8248dd5e6ce3ee0a829
darja134 <55285599+darja134@users.noreply.github.com>
2024-05-29 15:30:20 +0300

    Updated user.py (commented check in constructor, renamed methods)

--- lesson3/user.py
+++ lesson3/user.py
@@ -1,20 +1,20 @@
 class User:
     def __init__(self, first_name, last_name):
-        if(not first_name or not last_name):
-            raise ValueError("Value(s) should not be empty!")
-        else:
+        #if(not first_name or not last_name):
+        #    raise ValueError("Value(s) should not be empty!")
+        #else:
             self.first_name = first_name
             self.last_name = last_name
     
 
-    def printName(self):
+    def print_name(self):
         print(self.first_name)
 
 
-    def printSurname(self):
+    def print_surname(self):
         print(self.last_name)
 
 
-    def printNameAnsSurname(self):
+    def print_name_and_surname(self):
         print(self.first_name + " " + self.last_name)
 
