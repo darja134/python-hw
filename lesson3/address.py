26a7f13910af73b2962571c8751e5e73728d37d1
darja134 <55285599+darja134@users.noreply.github.com>
2024-05-29 15:01:32 +0300

    Updated address.py (commented fields, check in constructor)

--- lesson3/address.py
+++ lesson3/address.py
@@ -1,15 +1,15 @@
 class Address:
-    index = 000000
-    city = "City"
-    street= "St. Street"
-    building = 13
-    flat = 4
+    #index = 000000
+    #city = "City"
+    #street= "St. Street"
+    #building = 13
+    #flat = 4
 
 
     def __init__(self, address):
-        if(not address):
-            raise ValueError("Value(s) should not be empty!")
-        else:
+        #if(not address):
+        #    raise ValueError("Value(s) should not be empty!")
+        #else:
             self.index = address[0]
             self.city = address[1]
             self.street = address[2]
