32fc73aea70200d2c8346209b41eb423e3406c29
dasha <dasha_134@yahoo.ie>
2024-06-16 13:05:43 +0300

    Fixed all TODO's

--- lesson3/address.py
+++ lesson3/address.py
@@ -1,23 +1,17 @@
 class Address:
-    #index = 000000
-    #city = "City"
-    #street= "St. Street"
-    #building = 13
-    #flat = 4
 
-
-    def __init__(self, address):
+    def __init__(self, index, city, street, building, flat):
         #if(not address):
         #    raise ValueError("Value(s) should not be empty!")
         #else:
-            self.index = address[0]
-            self.city = address[1]
-            self.street = address[2]
-            self.building = address[3]
-            self.flat = address[4]
+            self.index = index
+            self.city = city
+            self.street = street
+            self.building = building
+            self.flat = flat
 
 
     def print_address(self):
         stringAddress = str(self.index) + ", " + self.city + ", " + self.street + ", " + \
-              str(self.building) + " - " + str(self.flat)
+              str(self.building) + "-" + str(self.flat)
         return stringAddress
